e5bfeb22a9e99affebf56202d20b9b3128c0c54e
AdrianaSaty <adri-saty@hotmail.com>
2024-01-18 15:07:53 -0300

    feat: implements api weather call

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
+const weatherForm = document.getElementById("weatherForm");
+const weatherDiv = document.getElementById("weather");
 
+weatherForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const city = event.target[0].value;
+    searchWeather(city);
+})
 
-async function searchWeather() {
-    await fetch('http://api.openweathermap.org/geo/1.0/direct?q=London&limit=1&appid=cc15b86641ceefed4f4b037128019183')
+async function searchWeather(city) {
+    const appKey = 'cc15b86641ceefed4f4b037128019183';
+    const lang = 'en';
+    const units = 'metric';
+    const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appKey}&lang=${lang}&units=${units}`);
+    const weatherData = await weatherResponse.json();
+    showWeather(weatherData);
 }
 
-searchWeather()
+function showWeather(weather) {
+    const temperature = Math.r(weather.main.temp);
+    const description = weather.weather[0].description;
+    const city = weather.name;
+    const iconCode = weather.weather[0].icon;
+    const iconURL = `https://openweathermap.org/img/wn/${iconCode}.png`;
+
+    weatherDiv.innerHTML = '';
+
+    const cityH2 = document.createElement("h2");
+    cityH2.textContent = city;
+
+    const iconImage = document.createElement("img");
+    iconImage.src = iconURL;
+    iconImage.alt = description;
+
+    const descriptionParagraph = document.createElement("p");
+    descriptionParagraph.textContent = description;
+
+    const temperatureParagraph = document.createElement("p");
+    temperatureParagraph.textContent = `Temperature: ${temperature}°C`;
+
+    weatherDiv.appendChild(cityH2);
+    weatherDiv.appendChild(iconImage);
+    weatherDiv.appendChild(descriptionParagraph);
+    weatherDiv.appendChild(temperatureParagraph);
+}
